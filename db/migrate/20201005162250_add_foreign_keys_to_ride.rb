6da7457334799b4123947edce8df2868857eb19a
Hannah Angle <hannahcangle@gmail.com>
2020-10-05 16:23:26 +0000

    Automatically backed up by Learn

--- db/migrate/20201005162250_add_foreign_keys_to_ride.rb
+++ db/migrate/20201005162250_add_foreign_keys_to_ride.rb
@@ -1,4 +1,4 @@
-class AddForeignKeysToRide < ActiveRecord::Migration[5.0]
+class AddForeignKeysToRide < ActiveRecord::Migration
   def change
     add_column :rides, :passenger_id, :integer
     add_column :rides, :taxi_id, :integer
